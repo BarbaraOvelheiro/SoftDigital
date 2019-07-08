0e78880636b3ea8ddb97849ecf514b99a5da14f7
Bárbara Ovelheiro <bovelheiro@hotmail.com>
2019-07-08 16:33:28 +0100

    Criação da class FileUpload. Ligação da aplicação ao Firebase. Adicionado a Cloud Storage do Firebase à aplicação.

---
+++ app/src/main/java/pt/ipg/a/softdigital/FileUpload.java
@@ -0,0 +1,15 @@
+package pt.ipg.a.softdigital;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FileUpload extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_upload);
+
+
+    }
+}
