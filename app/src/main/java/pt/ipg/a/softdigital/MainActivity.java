9651514d112a38db4eb90dd4cb085eaae421e7db
Bárbara Ovelheiro <bovelheiro@hotmail.com>
2019-07-14 16:45:10 +0100

    Criação de uma página inicial com todas as informações possíveis

--- app/src/main/java/pt/ipg/a/softdigital/MainActivity.java
+++ app/src/main/java/pt/ipg/a/softdigital/MainActivity.java
@@ -1,11 +1,13 @@
 package pt.ipg.a.softdigital;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -18,7 +20,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 import com.google.firebase.auth.UserInfo;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "MainActivity";
 
@@ -26,6 +28,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.add_file_button).setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        int i = view.getId();
+
+        if(i == R.id.add_file_button){
+            Intent intent = new Intent(this, FileUpload.class);
+            startActivity(intent);
+            finish();
+        }
+
+
+    }
 }
