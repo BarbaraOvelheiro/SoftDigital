98b6b243fe888bba9e4de43a93513980b82b212c
Bárbara Ovelheiro <bovelheiro@hotmail.com>
2019-09-14 18:54:31 +0100

    corrigido função para ler os documentos a aguardar por outros.

--- app/src/main/java/pt/ipg/a/softdigital/WaitForOthersDocumentsFragment.java
+++ app/src/main/java/pt/ipg/a/softdigital/WaitForOthersDocumentsFragment.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,13 +29,14 @@ import com.google.firebase.database.ValueEventListener;
  */
 public class WaitForOthersDocumentsFragment extends Fragment {
 
+    private static final String TAG = "WaitForOthersDocumentsFragment";
 
     private View WaitForOthersDocuments;
 
     private RecyclerView wait_for_others_documents_list;
 
     private FirebaseAuth mAuth;
-    private DatabaseReference UserRef, MessagesRef;
+    private DatabaseReference UserRef, MessagesRef, documentStatus, FilesRef;
 
     private String currentUserID;
 
@@ -57,6 +59,8 @@ public class WaitForOthersDocumentsFragment extends Fragment {
 
         MessagesRef = FirebaseDatabase.getInstance().getReference().child("Messages").child(currentUserID);
         UserRef = FirebaseDatabase.getInstance().getReference().child("User");
+        documentStatus = FirebaseDatabase.getInstance().getReference().child("Document Status").child(currentUserID);
+        FilesRef = FirebaseDatabase.getInstance().getReference().child("Files");
 
         return WaitForOthersDocuments;
     }
@@ -66,61 +70,129 @@ public class WaitForOthersDocumentsFragment extends Fragment {
     public void onStart() {
         super.onStart();
 
-        FirebaseRecyclerOptions options = new FirebaseRecyclerOptions.Builder<Messages>().setQuery(MessagesRef, Messages.class).build();
+        FirebaseRecyclerOptions options = new FirebaseRecyclerOptions.Builder<DocumentStatus>()
+                .setQuery(documentStatus, DocumentStatus.class).build();
 
-        FirebaseRecyclerAdapter<Messages, WaitForOthersDocumentsViewHolder> adapter
-                = new FirebaseRecyclerAdapter<Messages, WaitForOthersDocumentsViewHolder>(options) {
+        FirebaseRecyclerAdapter<DocumentStatus, WaitForOthersDocumentsViewHolder> adapter
+                = new FirebaseRecyclerAdapter<DocumentStatus, WaitForOthersDocumentsViewHolder>(options) {
             @Override
-            protected void onBindViewHolder(@NonNull final WaitForOthersDocumentsViewHolder holder, final int position, @NonNull final Messages model) {
+            protected void onBindViewHolder(@NonNull final WaitForOthersDocumentsViewHolder holder, final int position, @NonNull final DocumentStatus model) {
 
-                String fromUserID = model.getUserFromID();
+                String messageID = model.getMessageID();
+                String status = model.getStatus();
+                Log.v(TAG, "status: " +status);
+                if(status.equals("a_aguardar_por_outros")) {
 
-                holder.document_receive_name_editText.setText(model.getDocumentName());
+                    if (messageID == null) {
 
-                UserRef = FirebaseDatabase.getInstance().getReference().child("User").child(fromUserID);
-
-                UserRef.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        if (dataSnapshot.hasChild("userName")) {
-
-                            String receiverName = dataSnapshot.child("userName").getValue().toString();
-                            holder.send_userName_editText.setText(receiverName);
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
-                holder.itemView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-
-                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                        String documentName = model.getDocumentID();
+                        FilesRef = FirebaseDatabase.getInstance().getReference().child("Files").child(currentUserID).child(documentName);
+                        FilesRef.addValueEventListener(new ValueEventListener() {
                             @Override
-                            public void onClick(View view) {
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if (dataSnapshot.hasChild("documentName")) {
+                                    String docuName = dataSnapshot.child("documentName").getValue().toString();
+                                    holder.document_receive_name_editText.setText(docuName);
+                                    Log.v(TAG, "docuname:  " + docuName);
+                                }
+                                if(dataSnapshot.hasChild("documentUrl")){
+                                    final String url = dataSnapshot.child("documentUrl").getValue().toString();
+                                    holder.itemView.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
 
-                                // String click_document_id = getRef(position).getKey();
-                                String url = model.getDocumentUrl();
-                                String receiverPdfID = getRef(position).getKey();
+                                            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                                                @Override
+                                                public void onClick(View view) {
+                                                    String receiverPdfID = getRef(position).getKey();
+                                                    String receiverStatusID = model.getStatusID();
 
-                                Intent intent = new Intent(getActivity(), Pdf_view.class);
-                                intent.putExtra("pdfurl", url);
-                                intent.putExtra("receiverPdfID", receiverPdfID);
-                                startActivity(intent);
+                                                    Intent intent = new Intent(getActivity(), Pdf_view.class);
+                                                    intent.putExtra("pdfurl", url);
+                                                    intent.putExtra("receiverPdfID", receiverPdfID);
+                                                    intent.putExtra("receiverStatusID", receiverStatusID);
+                                                    startActivity(intent);
+                                                }
+                                            });
+
+                                        }
+                                    });
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
                             }
                         });
 
-                    }
-                });
+                    } else {
+                        holder.fromtextView.setVisibility(View.VISIBLE);
+                        holder.send_userName_editText.setVisibility(View.VISIBLE);
+                        holder.linearLayoutView.setVisibility(View.VISIBLE);
 
-                if(fromUserID.equals(currentUserID)) {
-                    holder.fromtextView.setVisibility(View.VISIBLE);
-                    holder.send_userName_editText.setVisibility(View.VISIBLE);
-                    holder.linearLayoutView.setVisibility(View.VISIBLE);
-                }else {
+                        MessagesRef = FirebaseDatabase.getInstance().getReference().child("Messages").child(messageID);
+                        MessagesRef.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if (dataSnapshot.hasChild("userFromID")) {
+                                    String userName = dataSnapshot.child("userFromID").getValue().toString();
+
+                                    UserRef = FirebaseDatabase.getInstance().getReference().child("User").child(userName);
+                                    UserRef.addValueEventListener(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                            if (dataSnapshot.hasChild("userName")) {
+
+                                                String receiverName = dataSnapshot.child("userName").getValue().toString();
+                                                holder.send_userName_editText.setText(receiverName);
+
+                                            }
+
+                                        }
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                        }
+                                    });
+                                }
+                                if(dataSnapshot.hasChild("documentName")){
+                                    String docuName = dataSnapshot.child("documentName").getValue().toString();
+                                    holder.document_receive_name_editText.setText(docuName);
+                                }
+                                if(dataSnapshot.hasChild("documentUrl")){
+                                    final String url = dataSnapshot.child("documentUrl").getValue().toString();
+                                    holder.itemView.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+
+                                            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                                                @Override
+                                                public void onClick(View view) {
+                                                    String receiverPdfID = getRef(position).getKey();
+                                                    String receiverStatusID = model.getStatusID();
+
+                                                    Intent intent = new Intent(getActivity(), Pdf_view.class);
+                                                    intent.putExtra("pdfurl", url);
+                                                    intent.putExtra("receiverPdfID", receiverPdfID);
+                                                    intent.putExtra("receiverStatusID", receiverStatusID);
+                                                    startActivity(intent);
+                                                }
+                                            });
+
+                                        }
+                                    });
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+                    }
+                }else{
                     holder.linearLayoutView.setVisibility(View.INVISIBLE);
                 }
             }
